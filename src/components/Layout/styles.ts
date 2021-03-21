5fabe761e3d1363a1a0cfb7e152818c7f84d306e
Mateus de Souza <mateusdesouzapaula97@gmail.com>
2021-03-21 19:36:07 -0300

    add: AdBanner component top margin

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -4,24 +4,34 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 
+  > span {
+    margin-top: 3rem;
+    display: flex;
+  }
+
   @media (min-width: 1180px) {
     > main {
-      display: flex;
-      align-items: start;
-      justify-content: center;
-
       margin: 0 2rem;
+      display: flex;
+      justify-content: center;
+    }
+
+    > span {
+      margin-top: 3.5rem;
+      padding: 0.5rem 0;
     }
   }
 
   .left-column,
-  .right-column {
+  .right-column,
+  .ad-banner {
     display: none;
   }
 
   @media (min-width: 1180px) {
     .left-column,
-    .right-column {
+    .right-column,
+    .ad-banner {
       display: unset;
     }
     .middle-column {
